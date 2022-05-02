743c50226c5652c85e82d601873ee25d8d1eec14
romeo <romeomeshack34@gmail.com>
2022-05-02 09:11:29 +0100

    Added model function to create data in database

--- models/productModels.js
+++ models/productModels.js
@@ -1,4 +1,6 @@
 const products = require("../data/products.json");
+const {v4: uuidv4} = require("uuid");
+const {writeDataToFile} = require("../utils")
 
 // @desc Get all products
 function findAll() {
@@ -15,6 +17,15 @@ function findById(id) {
    });
 }
 
+// @desc Create single product
+function create(product) {
+    const newProduct = {id: uuidv4(), ...product};
+    products.push(newProduct);
+    writeDataToFile("./data/products.json", products);
+
+    return new Promise((resolve, reject) => resolve(newProduct));
+}
 
 
-module.exports = {findAll, findById};
+
+module.exports = {findAll, findById, create};
