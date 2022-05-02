8a76738426452b6628a3f93aabafb066de465d3a
romeo <romeomeshack34@gmail.com>
2022-05-02 09:02:33 +0100

    Added create function to route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const PORT = process.env.PORT || 5000;
 
-const {getProducts, getProduct} = require("./controllers/productController");
+const {getProducts, getProduct, createProduct} = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
     const headerValues = {"Content-Type": "application/json"};
@@ -9,9 +9,11 @@ const server = http.createServer((req, res) => {
     // Get all products
     if (req.url === "/api/products" && req.method === "GET") {
         getProducts(req, res);
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/)[1] && req.method === "GET") {
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "GET") {
         const productId = Number(req.url.split("/")[3]);
         getProduct(req, res, productId);
+    } else if (req.url === "/api/products" && req.method === "POST") {
+        createProduct(req, res);
     } else {
         res.writeHead(404, headerValues);
         res.end(JSON.stringify({message: "Route not found"}));
