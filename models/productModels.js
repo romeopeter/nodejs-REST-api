462c3899a19b470a65dbad5fccd16907faeb7eac
romeo <romeomeshack34@gmail.com>
2022-05-15 03:06:48 +0100

    Added update and remove function

--- models/productModels.js
+++ models/productModels.js
@@ -26,6 +26,24 @@ function create(product) {
     return new Promise((resolve, reject) => resolve(newProduct));
 }
 
+// @desc Update single product
+function update(id, product) {  
+    return new Promise((resolve, reject) => {
+        const productIndex = products.findIndex(p => p.id === id);
+        products[productIndex] = {id, ...product};
+        writeDataToFile("./data/products.json", products);
+        console.log(products[productIndex]);
+        return resolve(products[productIndex]);
+    });
+}
 
+// @desc Delete single product
+function remove(id) {  
+    return new Promise((resolve, reject) => {
+        const existingProducts = products.filter(p => p.id !== id);
+        writeDataToFile("./data/products.json", existingProducts);
+        return resolve(existingProducts);
+    });
+}
 
-module.exports = {findAll, findById, create};
+module.exports = {findAll, findById, create, update, remove};
